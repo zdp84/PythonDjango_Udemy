66ca03ef58685b71c615ac295e83c97de5130d67
zdp_84 <zdp-mail@mail.ru>
2019-04-10 20:57:19 +0300

    DOM Tic Tac Toe Game

--- dom_TicTacToe_game/dom_TicTacToe_game.js
+++ dom_TicTacToe_game/dom_TicTacToe_game.js
@@ -1,17 +1,37 @@
+// Get all cells and button DOM
 var cells = document.querySelectorAll('td')
+var btn = document.querySelector('.btn')
 
-
+//convert cells NodeList to Array
+var objLst = []
 for (cell of cells){
-  cell.addEventListener('click',function(){
-    cell.textContent = "X";
-    cell.style.color = 'red';
-  })
-  // cell.textContent = "X";
-  console.log(cell);
+  objLst.push(cell)
 }
 
+//map the 'click' function to cells
+objLst.map(function(cell){
+  cell.addEventListener('click',function(){
+    var txt = cell.textContent;
+      switch (txt) {
+        case 'X':
+          cell.textContent = 'O'
+          break;
+        case 'O':
+          cell.textContent = ''
+          break;
+        case '':
+          cell.textContent = 'X'
+          break;
+        default:
+          cell.textContent = ''
+          break;
+      }
+  })
+})
 
-// cells.addEventListener('click',function(){
-//   cells.textContent = "X";
-//   // cells.style.color = 'red';
-// })
+//refresh table when 'Restart' pressed
+btn.addEventListener('click',function(){
+  objLst.map(function(cell){
+    cell.textContent = ''
+  })
+})
